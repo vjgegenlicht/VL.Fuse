cbb77f2d61b4a0127bb7b139dc598d840f0a2efd
gregsn <gregor@vvvv.org>
2021-08-31 00:47:49 +0200

    some more ideas regarding custom regions

---
+++ src/Fuse/VL_API_Sketches/CustomRegion_DelegateApproach.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using VL.Core;
+using VL.Lib.Collections;
+
+namespace Fuse.VL_API_Sketches.DelegateApproach
+{
+    public struct BorderControlPointDescription
+    {
+        public string Name;
+        public IVLTypeInfo TypeInfo;
+        public int Index;
+    }
+
+    public delegate void RegionPatchCreator(in Spread<object> inputs, out object stateOutput, out Spread<object> outputs);
+    public delegate void RegionPatchUpdator(in object stateInput, in Spread<object> inputs, out object stateOutput, out Spread<object> outputs);
+    //public delegate void RegionPatchDisposer(in object stateInput);
+
+    /// <summary>
+    /// Represents the application of your region by the user, the values that flow into the region and outof. 
+    /// It also allows you to instanciate what's inside: the patch of the user. 
+    /// </summary>
+    public class CustomRegion
+    {
+        public Spread<BorderControlPointDescription> Inputs { get; }
+        public Spread<BorderControlPointDescription> Outputs { get; }
+        public RegionPatchCreator PatchCreator;
+        public RegionPatchUpdator PatchUpdator;
+        //public RegionPatchDisposer PatchDisposer;
+
+        public Spread<object> ReadInputs()
+        {
+            return null;
+        }
+
+        public void WriteOutputs(IEnumerable<object> outputs)
+        {
+        }
+
+        public void CreateRegionPatch(NodeContext nodeContext, in Spread<object> inputs, out CustomRegionPatch regionPatch, out Spread<object> outputs)
+        {
+            regionPatch = new CustomRegionPatch(nodeContext, this);
+            outputs = null;
+        }
+    }
+
+    /// <summary>
+    /// Represents the body inside the region as patched by the user. 
+    /// </summary>
+    public struct CustomRegionPatch : IDisposable
+    {
+        object State;
+        CustomRegion Region;
+        NodeContext NodeContext;
+
+        public CustomRegionPatch(object State) : this()
+        {
+            this.State = State;
+        }
+
+        public CustomRegionPatch(NodeContext NodeContext, CustomRegion CustomRegion) : this()
+        {
+            this.NodeContext = NodeContext;
+            this.Region = CustomRegion;
+        }
+
+        public CustomRegionPatch Update(in Spread<object> inputs, out Spread<object> outputs)
+        {
+            Region.PatchUpdator(in State, in inputs, out var newState, out outputs);
+            if (NodeContext.IsImmutable)
+            {
+                if (newState != State)
+                    return new CustomRegionPatch(newState);
+                else
+                    return this;
+            }
+            else
+            {
+                State = newState;
+                return this;
+            }
+        }
+
+        public void Dispose()
+        {
+            (State as IDisposable)?.Dispose();
+        }
+    }
+}
---
+++ src/Fuse/VL_API_Sketches/CustomRegion_InterfaceApproach.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using VL.Core;
+using VL.Lib.Collections;
+
+namespace Fuse.VL_API_Sketches.InterfaceApproach
+{
+    public struct BorderControlPointDescription
+    {
+        public string Name;
+        public IVLTypeInfo TypeInfo;
+        public int Index;
+    }
+
+    /// <summary>
+    /// This represents the user patch inside the region
+    /// You may create and manage several patch states by calling CreateRegionPatch
+    /// </summary>
+    public interface ICustomRegionPatch
+    {
+        /// <summary>
+        /// Updates the patch state by calling what the user patched
+        /// If the context is immutable it will return a new instance of the type when necessary
+        /// </summary>
+        /// <param name="inputs">The inputs from the inside perspective</param>
+        /// <param name="outputs">The outputs from the inside perspective</param>
+        /// <returns></returns>
+        public ICustomRegionPatch Update(Spread<object> inputs, out Spread<object> outputs);
+    }
+
+    /// <summary>
+    /// Represents the application of your region by the user, the values that flow into the region and outof. 
+    /// It also allows you to instanciate what's inside: the patch of the user. 
+    /// </summary>
+    public interface ICustomRegion
+    {
+        public Spread<BorderControlPointDescription> Inputs { get; }
+        /// <summary>
+        /// The outputs from an outside perspective
+        /// </summary>
+        public Spread<BorderControlPointDescription> Outputs { get; }
+
+        /// <summary>
+        /// Retrieves the untouched inputs. 
+        /// Your region might want to change some values before feeding it to the patch.
+        /// </summary>
+        public Spread<object> ReadInputs();
+
+        /// <summary>
+        /// After updating the custom region patch and altering the values you finally need to write the outputs.
+        /// These may be different from the values that you got from the patch update call.
+        /// </summary>
+        /// <param name="outputs"></param>
+        public void WriteOutputs(IEnumerable<object> outputs);
+
+        /// <summary>
+        /// Create a patch state
+        /// </summary>
+        /// <param name="Context"></param>
+        /// <param name="inputs"></param>
+        /// <param name="outputs"></param>
+        /// <returns></returns>
+        public ICustomRegionPatch CreateRegionPatch(NodeContext Context, Spread<object> inputs, out Spread<object> outputs);
+    }
+}
